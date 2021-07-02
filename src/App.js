aeb727712cb695d237103f36f0dd7fddd500f561
devanshidesai03 <devanshi.engg@gmail.com>
2021-07-02 15:50:01 +0530

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { BrowserRouter,Route,Switch } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Projects from "./Pages/Projects";
+import background from "./image1.jpg";
+
+const App = ()  => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi I'm Devanshi Desai
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <BrowserRouter>
+       <Switch>
+       <Route path="/" exact>
+          <Homepage />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+       </Switch>
+    </BrowserRouter>
+  
+    
   );
 }
 
